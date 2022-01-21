910ca03fbbdae63bf66362498098359c44ba1753
Lorenzo Bucci <lorenzo.bucci@stud.unifi.it>
2022-01-21 15:26:19 +0100

    Implementazione Controller

---
+++ src/controller/Controller.py
@@ -0,0 +1,28 @@
+import threading
+
+from model.Model import Model
+from view.STUNATView import STUNATView
+
+
+class Controller:
+    def __init__(self, stuNatView: STUNATView, model: Model):
+        self.view = stuNatView
+        self.model = model
+        startButtonHandlerThread = threading.Thread(target=self._startButtonHandler)
+
+        self.view.startButton.clicked.connect(
+            lambda: {
+                self.view.setNatRepresentation("Loading.gif"),
+                startButtonHandlerThread.start()
+            }
+        )
+        self.view.sourceIPComboBox.insertItems(0, self.model.localIPList)
+        self.view.sourceIPComboBox.setCurrentIndex(0)
+
+    def _startButtonHandler(self):
+        self.model.startTest(self.view.serverHostnameField.text(), self.view.serverPortField.text(),
+                             self.view.sourceIPComboBox.currentText(), self.view.localPortField.text())
+        self.view.setNatRepresentation(self.model.testResults["natRepresentationImage"])
+        self.view.natTypeResultLabel.setText(self.model.testResults["natType"])
+        self.view.extIPResultLabel.setText(self.model.testResults["extIP"])
+        self.view.extPortResultLabel.setText(str(self.model.testResults["extPort"]))
--- src/main.py
+++ src/main.py
@@ -2,9 +2,13 @@ import sys
 
 from PyQt5 import QtWidgets
 
+from controller.Controller import Controller
+from model.Model import Model
 from src.view.STUNATView import STUNATView
 
 app = QtWidgets.QApplication(sys.argv)
+model = Model()
 mainWindow = STUNATView()
+controller = Controller(mainWindow, model)
 mainWindow.show()
 sys.exit(app.exec_())
