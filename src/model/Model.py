2485db2a09e2683ffa7520f1c6432ed656e6ad82
Lorenzo Bucci <lorenzo.bucci@stud.unifi.it>
2022-03-13 15:04:13 +0100

    Correzione minore sul modello

--- src/model/Model.py
+++ src/model/Model.py
@@ -6,7 +6,7 @@ import stun
 from model.STUNLogger import STUNLogger
 
 
-def _createSocket(ip, port):
+def createUDPSocket(ip, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.settimeout(2)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -15,15 +15,17 @@ def _createSocket(ip, port):
 
 
 class Model:
+
     def __init__(self):
         self._logger = STUNLogger()
         self.rawLog = ""
 
-        self._socket = _createSocket("0.0.0.0", 0)
+        self._socket = createUDPSocket("0.0.0.0", 0)
         self.localPort = self._socket.getsockname()[1]
 
         self.testResults = None
         self.localIPList = ["Default"]
+
         for iface in netifaces.interfaces():
             ifaceDetails = netifaces.ifaddresses(iface)
             if netifaces.AF_INET in ifaceDetails:
@@ -41,7 +43,7 @@ class Model:
 
         self._socket.close()
         self.localPort = int(localPort)
-        self._socket = _createSocket(_sourceIP, self.localPort)
+        self._socket = createUDPSocket(_sourceIP, self.localPort)
 
         res = stun.get_nat_type(self._socket, _sourceIP, self.localPort, serverHostname, int(serverPort))
         self.testResults = resultsMap[res[0]]
@@ -56,7 +58,7 @@ resultsMap = {
         "isAnError": True,
         "errorName": "Connection blocked",
         "errorDescription": "Unable to reach STUN server.\nPlease, check your connection and try again.",
-        "natRepresentationImage": "Blocked.png",
+        "natRepresentationImage": "Blocked.png"
     },
     "Open Internet": {
         "isAnError": False,
@@ -92,6 +94,6 @@ resultsMap = {
         "isAnError": True,
         "errorName": "Changed address error",
         "errorDescription": "Meet an error, when do Test1 on Changed IP and Port. Try change STUN server.",
-        "natRepresentationImage": "UnknownNAT.png",
+        "natRepresentationImage": "UnknownNAT.png"
     }
 }
